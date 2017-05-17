b2190c8fc743660ce041a7de3b3c9a3b0e7897f3
Mads Kristensen <post@madskristensen.net>
2017-05-17 11:39:26 -0700

    Updated build script

--- build/build.ps1
+++ build/build.ps1
@@ -1,4 +1,8 @@
+Push-Location
+cd build
+
 $url = "https://www.nuget.org/api/v2/package/Microsoft.AspNetCore.SpaTemplates/0.9.2"
 $output = "..\src\Microsoft.AspNetCore.SpaTemplates.nupkg"
 
 Invoke-WebRequest -Uri $url -OutFile $output
+Pop-Location
