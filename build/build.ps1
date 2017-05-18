f5ab6d13a59556f8a795a4a7b63646848388110b
Mads Kristensen <post@madskristensen.net>
2017-05-18 08:03:14 -0700

    Updated build

--- build/build.ps1
+++ build/build.ps1
@@ -1,8 +1,14 @@
+$package = "Microsoft.AspNetCore.SpaTemplates"
+$version = "0.9.3"
+
 Push-Location
 cd build
 
-$url = "https://www.nuget.org/api/v2/package/Microsoft.AspNetCore.SpaTemplates/0.9.2"
-$output = "..\src\Microsoft.AspNetCore.SpaTemplates.nupkg"
+# Delete all existing .nupkg files
+dir ..\src\*.nupkg | foreach {del $_}
+
+$url = "https://www.nuget.org/api/v2/package/$package/$version"
+$output = "..\src\$package.$version.nupkg"
 
 Invoke-WebRequest -Uri $url -OutFile $output
 Pop-Location
